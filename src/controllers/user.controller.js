d1e98993aae7b6d7e3aaf3ade4318e9c944f4af0
DaniloNogueira-Silva <dansilva730x@gmail.com>
2023-04-14 13:55:02 -0300

    feat: Criar controllers da api

---
+++ src/controllers/user.controller.js
@@ -0,0 +1,55 @@
+import bcrypt from "bcrypt";
+import { userValidation } from '../validations/user.validation'
+import { createUser, deleteUser, getAll, getById, updateUser } from "../repositorys/user.repository";
+
+export const create = async (req, res) => {
+    try {
+
+        await userValidation.validate(req.body)
+
+        //cript password
+        const hashPassword = await bcrypt.hash(req.body.password, 10)
+        req.body.password = hashPassword;
+        
+        const user = await createUser(req.body);
+        res.status(200).send(user)
+    } catch (error) {
+        res.status(400).send(error);
+    }
+};
+
+export const get = async (req,res) => {
+    try {
+        const users = await getAll();
+        res.status(200).send(users);
+    } catch (error) {
+        res.status(400).send(e);
+    }
+}
+
+export const getId = async (req,res) => {
+    try {
+        const user = await getById(Number(req.params.id))
+        res.status(200).send(user)
+    } catch (error) {
+        res.status(400).send(error);
+    }
+};
+
+export const update = async(req,res) => {
+    try {
+        const user = await updateUser(Number(req.params.id), req.body)
+        res.status(200).send(user)
+    } catch (error) {
+        res.status(400).send(error);
+    }
+};
+
+export const remove = async (req,res) => {
+    try {
+        await deleteUser(Number(req.params.id));
+        res.send(200).send()
+    } catch (error) {
+        res.status(400).send(error)
+    }
+}
